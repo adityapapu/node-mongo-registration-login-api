6131e550175aa6723efb069669151c7334293506
adityapapu <adityapanda1001@gmail.com>
2021-09-08 20:07:27 +0530

    final update

--- users/users.controller.js
+++ users/users.controller.js
@@ -7,21 +7,25 @@ router.post('/authenticate', authenticate);
 router.post('/register', register);
 router.get('/', getAll);
 router.get('/current', getCurrent);
+router.put('/current', putCurrent);
+
 router.get('/:id', getById);
 router.put('/:id', update);
 router.delete('/:id', _delete);
 
 module.exports = router;
 
-function authenticate(req, res, next) {
+function authenticate(req, res, next) 
+{
     userService.authenticate(req.body)
-        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .then(user => user ? res.json({"success":1,"data":user}) : res.status(400).json({ message: 'email or password is incorrect' }))
         .catch(err => next(err));
 }
 
-function register(req, res, next) {
+function register(req, res, next) 
+{
     userService.create(req.body)
-        .then(() => res.json({}))
+        .then(() => res.json({"success":1,"message":"User successfully created"}))
         .catch(err => next(err));
 }
 
@@ -32,8 +36,16 @@ function getAll(req, res, next) {
 }
 
 function getCurrent(req, res, next) {
+    console.log("getCurrent")
     userService.getById(req.user.sub)
-        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .then(user => user ? res.json({"success":1,"data":user}) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function putCurrent(req, res, next) {
+    console.log("putCurrent")
+    userService.update(req.user.sub,req.body)
+        .then(user => res.json({"success":1,"message":"User successfully updated"}))
         .catch(err => next(err));
 }
 
@@ -45,12 +57,12 @@ function getById(req, res, next) {
 
 function update(req, res, next) {
     userService.update(req.params.id, req.body)
-        .then(() => res.json({}))
+        .then(() => res.json({"success":1,"message":"User successfully updated"}))
         .catch(err => next(err));
 }
 
 function _delete(req, res, next) {
     userService.delete(req.params.id)
-        .then(() => res.json({}))
+        .then(() => res.json({"success":1,"message":"User successfully delete"}))
         .catch(err => next(err));
 }
